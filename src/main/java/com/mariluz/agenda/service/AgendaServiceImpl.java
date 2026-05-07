91b2967ca5c89004f867d385f347362bc0a5e063
bruno0416 <brunomcalderonv@gmail.com>
2026-05-06 20:51:08 -0400

    helper para detectar rol

--- src/main/java/com/mariluz/agenda/service/AgendaServiceImpl.java
+++ src/main/java/com/mariluz/agenda/service/AgendaServiceImpl.java
@@ -2,10 +2,14 @@ package com.mariluz.agenda.service;
 
 import com.mariluz.agenda.dto.AgendaConfigRequest;
 import com.mariluz.agenda.dto.AgendaConfigResponse;
+import com.mariluz.agenda.exceptions.UnauthorizedOperationException;
 import com.mariluz.agenda.model.AgendaConfig;
+import com.mariluz.agenda.model.User;
 import com.mariluz.agenda.repository.AgendaConfigRepository;
 import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +20,9 @@ public class AgendaServiceImpl implements AgendaService {
 
     @Override
     public AgendaConfigResponse configAgenda(AgendaConfigRequest request) {
-        // 1. guardar configuracion
+        // Validar rol del usuario
+        validateAdminAcces();
+        // 2. guardar configuracion
         AgendaConfig agendaConfig = agendaConfigRepo.save(
             AgendaConfig.builder()
                 .id(1) // guardamos con el id 1 para no crear una nueva tupla
@@ -29,7 +35,7 @@ public class AgendaServiceImpl implements AgendaService {
                 .build()
         );
 
-        // 2. retornar entidad guardada
+        // 3. retornar entidad guardada
         return AgendaConfigResponse.builder()
             .startWorkTime(agendaConfig.getStartWorkTime())
             .endWorkTime(agendaConfig.getEndWorkTime())
@@ -37,5 +43,28 @@ public class AgendaServiceImpl implements AgendaService {
             .breakTime(agendaConfig.getBreakTime())
             .workDays(agendaConfig.getWorkDays())
             .build();
+    }
+
+    // ------------------ Helper para validar rol usuario -------------------
+    private User getCurrentUser() {
+        Authentication auth =
+            SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null || !(auth.getPrincipal() instanceof User user)) {
+            throw new UnauthorizedOperationException(
+                "No hay un usuario autenticado"
+            );
+        }
+
+        return user;
+    }
+
+    private void validateAdminAcces() {
+        User user = getCurrentUser();
+
+        if (!"ADMIN".equalsIgnoreCase(user.getRole())) {
+            throw new UnauthorizedOperationException(
+                "Solo un administrador puede configurar la agenda"
+            );
+        }
     }
 }
