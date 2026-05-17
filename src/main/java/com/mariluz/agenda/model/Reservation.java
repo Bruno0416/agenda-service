a9f85e870d68eee33c732af0777ebe529cef498b
Bruno0416 <brunomcalderonv@gmail.com>
2026-05-17 17:06:42 -0400

    correccion enum status

--- src/main/java/com/mariluz/agenda/model/Reservation.java
+++ src/main/java/com/mariluz/agenda/model/Reservation.java
@@ -6,6 +6,8 @@ package com.mariluz.agenda.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -36,5 +38,6 @@ public class Reservation {
     private AgendaSlot agendaSlot;
 
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Status status;
 }
