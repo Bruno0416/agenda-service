f5225178e941718c79e5d6df7e78bb7de9f4e734
Bruno0416 <brunomcalderonv@gmail.com>
2026-05-18 17:12:08 -0400

    comentario sobre notification service

--- src/main/java/com/mariluz/agenda/client/NotificationClient.java
+++ src/main/java/com/mariluz/agenda/client/NotificationClient.java
@@ -1,6 +1,12 @@
 /*
-Servicio dedicado a comunicarse con MS-Notifications -> usando  RestClient
-RestClient es mas nuevo y tiene mejor soporte para las nuevas versiones de Spring
+    Servicio dedicado a comunicarse con MS-Notifications -> usando  RestClient
+    RestClient es mas nuevo y tiene mejor soporte para las nuevas versiones de Spring
+
+    NOTA: Ya que el servicio de resend no tiene
+    configurado un dominio (correo oficial de empresa/tienda)
+    el correo unicamente puede ser enviado al duenio de la api(brunomcalderonv@gmail.com)
+    -> por lo que cualquier intento de enviar correo a otra direccion va a  dar error
+
 */
 
 package com.mariluz.agenda.client;
